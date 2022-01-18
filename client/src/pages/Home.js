d847d8d79898a307e5c47507067422a3d416c03c
tiahwang44 <tiahwang44@gmail.com>
2022-01-18 22:06:12 +0900

    ADDED: Footer

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,3 +1,4 @@
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 const Home = () => {
@@ -5,6 +6,7 @@ const Home = () => {
     <>
       {/* {일단 임의로 isLoggedIn 값을 true/false로 주어서 Header부분 작동여부 확인} */}
       <Header isLoggedIn={true} />
+      <Footer />
     </>
   );
 };
