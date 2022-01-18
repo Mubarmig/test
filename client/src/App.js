3a4a3a5ae673de9443454604ced0f3addc1bbb4b
tiahwang44 <tiahwang44@gmail.com>
2022-01-18 21:20:12 +0900

    ADDED: Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Mypage from './pages/Mypage';
+import SignIn from './pages/SignIn';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/mypage/userInfo" element={<Mypage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
