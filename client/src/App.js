6325ae8ff1c88dca533069d37cb0a702fae63725
Sean Kim <89332470+seanswim@users.noreply.github.com>
2022-01-18 23:11:40 +0900

    Master (#9)
    * test Readme.md
    
    * ADDED: html index.js
    
    * hello
    
    * first commit
    
    * dev branch first commit
    
    * ADDED: signin.js
    
    * ADDED: signout.js (#7)
    
    Co-authored-by: tiahwang44 <tiahwang44@gmail.com>
    
    * hello (#8)
    
    Co-authored-by: Jin-hyeok_Heo <hwlsgur1120@naver.com>
    
    * CREATED: client/server folder
    
    * created react-app bootstrap
    
    * created react app
    
    * CREATED: server folder
    
    * ADDED: dependencies
    
    * ADDED: react-router-dom dependency
    
    * FIXED: delete default react files
    
    * ADDED: Header
    
    * CREATED: Server & DB build env
    
    * ADDED: Footer
    
    * CREATED: Sign up
    
    Co-authored-by: tiahwang44 <tiahwang44@gmail.com>
    Co-authored-by: Tia Hwang <90431155+tiatiahwang@users.noreply.github.com>
    Co-authored-by: Jin-hyeok_Heo <hwlsgur1120@naver.com>
    Co-authored-by: Mubarmig <throughworld97@gmail.com>
    Co-authored-by: Mubarmig <87626133+Mubarmig@users.noreply.github.com>
    Co-authored-by: SEB_Full_35_허진혁 <91868151+Jin-hyeok2@users.noreply.github.com>

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Mypage from './pages/Mypage';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/mypage/userInfo" element={<Mypage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
